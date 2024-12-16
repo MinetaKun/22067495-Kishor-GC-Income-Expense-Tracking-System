a98dacfbefef16f6ffb6648d2df364206680aa4b
kis-hor <102669996+kis-hor@users.noreply.github.com>
2024-12-16 20:37:09 +0545

    added

--- obj/Debug/net9.0-maccatalyst/maccatalyst-x64/22067495-Kishor-GC-Income-Expense-Tracking-App.AssemblyInfo.cs
+++ obj/Debug/net9.0-maccatalyst/maccatalyst-x64/22067495-Kishor-GC-Income-Expense-Tracking-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("22067495-Kishor-GC-Income-Expense-Tracking-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cfed6dd9510490e28287d614bb3a6bbc60208ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("22067495-Kishor-GC-Income-Expense-Tracking-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("22067495-Kishor-GC-Income-Expense-Tracking-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
